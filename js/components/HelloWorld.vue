6c0ce8dadc81edf230d30ab759cad9202e225ed6
Sergii Bida <sergijbida15@gmail.com>
2024-08-08 17:52:09 +0300

    did tasks 4, 5

---
+++ js/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <p class="title">
+    {{ title }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Hello World'
+    }
+  },
+}
+</script>
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
--- js/main.js
+++ js/main.js
@@ -3,9 +3,53 @@ const App = {
     name: 'App',
     data() {
         return {
-            header: 'Welcome!'
+            header: 'Welcome!',
+            counter: 0
         }
-    }
+    },
+    components: {
+        'HelloWorld': Vue.defineAsyncComponent(() => loadModule('./js/components/HelloWorld.vue', options))
+    },
 };
 
 Vue.createApp(App).mount('#app');
+
+
+// Для загрузки компонентов, если Vue через CDN
+const options = {
+    moduleCache: {
+        vue: Vue
+    },
+    async getFile(url) {
+
+        const res = await fetch(url);
+        if (!res.ok)
+            throw Object.assign(new Error(res.statusText + ' ' + url), {res});
+        return {
+            getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
+        }
+    },
+    addStyle(textContent) {
+
+        const style = Object.assign(document.createElement('style'), {textContent});
+        const ref = document.head.getElementsByTagName('style')[0] || null;
+        document.head.insertBefore(style, ref);
+    },
+}
+
+const {loadModule} = window['vue3-sfc-loader'];
+
+// Завдання 5: Створіть окремий файл з компонентом, зареєструйте та додайте до проєкту.
+const Components = {
+    name: 'Components',
+    components: {
+        'HelloWorld': Vue.defineAsyncComponent(() => loadModule('./js/components/HelloWorld.vue', options))
+    },
+    template: `
+        <div>
+            <HelloWorld/>
+        </div>
+    `
+};
+
+Vue.createApp(Components).mount('#components');
