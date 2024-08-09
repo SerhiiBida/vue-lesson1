9686da7db2598d461c102d0c4b23eaeccc3c20fe
Sergii Bida <sergijbida15@gmail.com>
2024-08-09 14:25:03 +0300

    added Counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <!--Завдання 4: Додайте в існуючий проект кнопку, яка буде змінювати значення вашої змінної в об'єкті Vue при кожному кліку.-->
+  <div>
+    <h2>
+      {{ counter }}
+    </h2>
+    <button @click="counter--">
+      -
+    </button>
+    <button @click="counter++">
+      +
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
+import {options} from './js/loadingComponents.js'
+
+const {loadModule} = window['vue3-sfc-loader'];
+
 // Завдання 2: Створіть основний об'єкт Vue і прив'яжіть його до деякого елементу HTML на вашій сторінці.
 const App = {
     name: 'App',
     data() {
         return {
-            header: 'Welcome!',
-            counter: 0,
+            header: 'Welcome!'
         }
-    }
+    },
+    components: {
+        'Counter': Vue.defineAsyncComponent(() => loadModule('./src/components/Counter.vue', options))
+    },
+    template: `
+        <header>
+            <!--Завдання 3: Створіть змінну в об'єкті Vue і виведіть її значення на сторінці.-->
+            <h1>
+                {{ header }}
+            </h1>
+        </header>
+    
+        <main>
+            <!--Завдання 4: Додайте в існуючий проект кнопку, яка буде змінювати значення вашої змінної в об'єкті Vue при кожному кліку.-->
+            <Counter/>
+            <div id="components">
+            
+            </div>
+        </main>
+    `
 };
 
 Vue.createApp(App).mount('#app');
